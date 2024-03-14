5705ae8c103a79604afd73ce55f3cbe03e869e62
Eh0802 <ehsoe0802@gmail.com>
2024-03-14 00:56:17 -0400

    Wee4

--- index.js
+++ index.js
@@ -2,37 +2,52 @@
 // https://pokeapi.co/api/v2/pokemon/pikachu
 
 
-// fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
-//     // .then(response => console.log(response))
-//     .then(response => {
-        
-//         if(!response.ok) {
-//             throw new Error ("Pokemon not found")
-//         }
-//         return response.json();
-//     })
-//     .then(data => console.log(data.name))
+// fetch("https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0")
+//     .then(response => response.json())
+//     .then(data => console.log(data))
 //     .catch(error => console.error(error));
 
-fetchDate();
+// fetchDate();
 
-async function fetchDate() {
-    try {
-        const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
-        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
-        // console.log(pokemonName)
+// async function fetchDate() {
+//     try {
+//         const namePokemon = document.getElementById("pokemonName").value.toLowerCase();
+//         const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${namePokemon}`)
+//         // console.log(response)
+//         if(!response.ok) {
+//             throw new Error("Pokemon not found")
+//         }
+//         const data = await response.json();
+//         console.log(data)
+//         const pokemonSprite = data.sprites.front_default;
+//         const imgElement = document.getElementById("pokemonSprite");
+//         imgElement.src = pokemonSprite;
+//         imgElement.style.display = "block"
 
-        if(!response.ok) {
-            throw new Error("Pokemon not found")
-        }
-        const data = await response.json();
-        const pokemonSprite = data.sprites.front_default;
-        const imgElement = document.getElementById("pokemonSprite");
-        imgElement.src = pokemonSprite;
-        imgElement.style.display = "block"
+//     }
+//     catch(error){
+//         console.error(error);
+//     }
 
-    }
-    catch(error){
-        console.error(error);
-    }
-}
+
+// }
+
+
+
+// async function fetchData() {
+//     const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0")
+//     const pokemonName = await response.json();
+//     console.log(
+//     pokemonName.map(
+//         (pokemon) => `<div class="container">
+//     <img src="" alt="sprites">
+//     <h3 class="name">name:</h3>
+//     <h4 class="id">id:</h4>
+//     <h4 class="types">types:</h4>` 
+//     )
+//         .join("")
+//     );
+
+// }
+
+// fetchData();
