91b37ab735e6bd721e932d4116b1ef93187d9d1c
Eh0802 <ehsoe0802@gmail.com>
2024-03-20 03:04:53 -0400

    week4

--- About.js
+++ About.js
@@ -1,33 +1,36 @@
 
 
 
-const pokemonSprites = document.querySelector(" .pokemin__img")
+const pokemonSprites = document.querySelector(" .pokemon__img")
 const pokemonName = document.querySelector(" .name")
 const pokemonId = document.querySelector(" .id")
 const pokemonType = document.querySelector(" .type")
 
 async function fetchData() {
-    const response = await fetch("https://pokeapi.co/api/v2/pokemon/1");
+
+
+    const namePokemon = document.getElementById("pokemonName").value.toLowerCase();
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/30`)
     const data = await response.json();
+    console.log(response)
     console.log(data)
 
-    pokemonSprites.src = data.sprites.front_default;
+    pokemonSprites.src = data.sprites.front_default
     pokemonName.innerHTML = data.name
     pokemonId.innerHTML = data.id
-    pokemonType.innerHTML = data.types.ytpe
+    pokemonType.innerHTML = data.types
 
 }
 
 fetchData()
 
-// console.log('hello')
 
 // fetchDate();
 
 // async function fetchDate() {
 //     try {
 //         const namePokemon = document.getElementById("pokemonName").value.toLowerCase();
-//         const response = await fetch(`https://pokeapi.co/api/v2/pokemon/pikachu`)
+//         const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${namePokemon}`)
 //         console.log(response)
 //         if(!response.ok) {
 //             throw new Error("Pokemon not found")
@@ -38,6 +41,7 @@ fetchData()
 //         const imgElement = document.getElementById("pokemonSprite");
 //         imgElement.src = pokemonSprite;
 //         imgElement.style.display = "block"
+//         const pokemonId = document.getElementById("id")
 
 //     }
 //     catch(error){
